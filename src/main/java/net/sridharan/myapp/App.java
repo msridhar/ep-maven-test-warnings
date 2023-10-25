f5d01fffb19afc212ac020ff991b7e6457bb4947
Manu Sridharan <msridhar@gmail.com>
2023-10-25 14:56:34 +0100

    add NullAway

--- src/main/java/net/sridharan/myapp/App.java
+++ src/main/java/net/sridharan/myapp/App.java
@@ -15,6 +15,8 @@ public class App
          System.out.println("whoops");
         }
         List<String> l = new ArrayList();
+        Object x = null;
+        int ignored = x.hashCode();
         System.out.println( "Hello World!" );
     }
 }
