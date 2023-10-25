e1ad19413a55b77b4e70109343fd465597b25144
Manu Sridharan <msridhar@gmail.com>
2023-10-25 14:48:06 +0100

    WIP

--- src/main/java/net/sridharan/myapp/App.java
+++ src/main/java/net/sridharan/myapp/App.java
@@ -1,5 +1,7 @@
 package net.sridharan.myapp;
 
+import java.util.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,11 @@ public class App
 {
     public static void main( String[] args )
     {
+        if (2 < 1) {
+         // Some code I don't want to run right now
+         System.out.println("whoops");
+        }
+        List<String> l = new ArrayList();
         System.out.println( "Hello World!" );
     }
 }
